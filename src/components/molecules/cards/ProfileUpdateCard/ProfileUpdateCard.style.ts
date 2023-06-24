917c17939106b855b8ae21e137a18925fcda4553
HoCG <rhwkd5012@naver.com>
2023-06-24 22:59:02 +0900

    feat: 프로필 업데이트 카드 구현

---
+++ src/components/molecules/cards/ProfileUpdateCard/ProfileUpdateCard.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { vw } from "rxn-units";
+
+export const profileUpdateCardStyles = StyleSheet.create({
+  cardTitle: {
+    color: "black",
+    fontSize: 17
+  },
+  container: {
+    borderRadius: 10,
+    backgroundColor: "#D9D9D9",
+    padding: 10
+  }
+});
