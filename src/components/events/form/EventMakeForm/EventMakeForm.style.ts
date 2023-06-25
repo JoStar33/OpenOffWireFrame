427bcd5f264d847751f6f678732f69efd0a7ecb6
HoCG <rhwkd5012@naver.com>
2023-06-26 02:25:50 +0900

    feat: 이벤트 생성 폼 구축

---
+++ src/components/events/form/EventMakeForm/EventMakeForm.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const eventMakeFormStyles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flexDirection: "column",
+    flex: 1,
+    backgroundColor: "black"
+  },
+  title: {
+    color: "white",
+    fontSize: 18
+  }
+});
---
+++ src/components/events/form/EventMakeForm/EventMakeForm.tsx
@@ -0,0 +1,55 @@
+import FormValidateInput from "components/atoms/inputs/FormValidateInput/FormValidateInput";
+import RadioButtonGroup from "components/molecules/groups/RadioButtonGroup/RadioButtonGroup";
+import { organizerCategory } from "data/forms/organizerCategory";
+import React from "react";
+import {
+  useForm,
+} from "react-hook-form";
+import { View, Text } from "react-native";
+import { validateName } from "utils/validate";
+import { eventMakeFormStyles } from "./EventMakeForm.style";
+
+const EventMakeForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  return (
+    <View style={eventMakeFormStyles.container}>
+      <Text style={eventMakeFormStyles.title}>주최자 정보를 입력해주세요</Text>
+      <FormValidateInput
+        model={{
+          type: "text",
+          name: "organizer",
+          label: "주최자 이름",
+          required: {
+            value: true,
+            message: "필수 입력 사항입니다!",
+          },
+          validate: (value: string) => validateName(value),
+        }}
+        errors={errors}
+        control={control}
+        color={"black"}
+      />
+      <RadioButtonGroup
+        radioElements={organizerCategory}
+        model={{
+          type: "radiogroup",
+          name: "organizerCategory",
+          label: "주최자 구분",
+          required: {
+            value: true,
+            message: "필수 입력 사항입니다!",
+          },
+          validate: (value: string) => undefined,
+        }}
+        errors={errors}
+        control={control}
+      />
+    </View>
+  );
+};
+
+export default EventMakeForm;
