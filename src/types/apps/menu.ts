49fdafa346dda145936e263c48d2482f93cd6519
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-06 18:11:22 +0900

    feat: 회원가입 화면에 필요한 타입값 추가

---
+++ src/types/apps/menu.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+  Login: undefined;
+  AgreeToTerm: undefined;
+  PhoneCertification: undefined;
+  EmailPassword: undefined;
+  NickName: undefined;
+  UserInfo: undefined;
+  BirthDay: undefined;
+  InterestField: undefined;
+};
---
+++ src/types/join.ts
@@ -0,0 +1,18 @@
+interface Action {
+  type: string;
+  value: string | string[];
+};
+
+interface JoinInfo {
+  name: string;
+  birth: string;
+  agreeToTerm: string;
+  phoneNumber: string;
+  gender: string;
+  emailAddress: string;
+  password: string;
+  interestField: string[];
+};
+
+export type { Action, JoinInfo };
+
