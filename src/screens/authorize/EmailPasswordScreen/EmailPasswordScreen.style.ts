304b0d2eaa92db86ef090f13cdac3e15b7a5801d
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-06 18:12:59 +0900

    feat: 이메일 패스워드 화면 구현

---
+++ src/screens/authorize/EmailPasswordScreen/EmailPasswordScreen.style.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+export const emailPasswordScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  }
+});
---
+++ src/screens/authorize/EmailPasswordScreen/EmailPasswordScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const EmailPasswordScreen = () => {
+  return (
+    <View>
+      <Text>이메일과 비밀번호를 입력해주세요.</Text>
+    </View>
+  );
+};
+
+export default EmailPasswordScreen;
