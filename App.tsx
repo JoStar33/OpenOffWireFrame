17b44a042d85a7ce787176ccb1bee4cec146663f
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-07-06 18:09:25 +0900

    feat: 이미지 및 스토리북 설정과 네비게이션 설정

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 // import StorybookUIRoot from "./.ondevice/Storybook";
 // export { StorybookUIRoot as default };
 import { NavigationContainer } from "@react-navigation/native";
+import AuthorizeNavigator from "navigators/AuthorizeNavigator";
 import Navigator from "navigators/Navigator";
+import { useState } from "react";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
   return (
     <NavigationContainer>
-      <Navigator />
+      {
+        isLogin ? <Navigator /> : <AuthorizeNavigator/>
+      }
     </NavigationContainer>
   );
 };
